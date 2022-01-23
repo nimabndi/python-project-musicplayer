6525f77083f319e31b5d263dbfd511119039bf23
nimabandi <nimabandi0935@gmail.com>
2022-01-23 12:19:34 -0800

    changed control panel

--- MP.py
+++ MP.py
@@ -22,6 +22,20 @@ showStatus = tk.Label(trackFrame, bg='white', fg='black', width=15)
 showStatus.grid(row=0, column=1, padx=20)
 
 # Control Panel
+Cpanel = tk.LabelFrame(root, text='Control Panel', bg='black', fg='white', font=('Arial', 10), bd=5, relief=tk.GROOVE,
+                       padx=15)
+Cpanel.place(x=10, y=120, width=580, height=90)
 
+play = tk.Button(Cpanel, text='play', width=15)
+play.grid(row=0, column=0, padx=10, pady=20)
+
+stop = tk.Button(Cpanel, text='stop', width=15)
+stop.grid(row=0, column=1, padx=10, pady=20)
+
+unpausing = tk.Button(Cpanel, text='unpausing', width=15)
+unpausing.grid(row=0, column=2, padx=10, pady=20)
+
+pausing = tk.Button(Cpanel, text='pausing', width=15)
+pausing.grid(row=0, column=3, padx=10, pady=20)
 
 root.mainloop()
