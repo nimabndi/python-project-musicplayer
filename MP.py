4c308c27e66c7b60c464963a6fb4cf3efc7ae374
nimabandi <nimabandi0935@gmail.com>
2022-01-23 11:22:57 -0800

    changed MP song track

--- MP.py
+++ MP.py
@@ -1,9 +1,21 @@
 import tkinter as tk
+
 import pygame as pg
 
 root = tk.Tk()  # make instance of tkinter
 root.title('Music Player')  # title of root
 root.geometry('600x400')  # select the of root
 root.resizable(False, False)  # fixed the size
+
+# Play List
+
+
+# Song Track
+
+trackFrame = tk.LabelFrame(root, text='Song Track', bg='black', fg='white', font=('Arial', 10), bd=5, relief=tk.GROOVE)
+trackFrame.place(x=10, y=10, width=580, height=90)
+
+# Control Panel
+
 
 root.mainloop()
