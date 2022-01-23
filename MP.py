656b1e55817b8d7dbef2e90a79655a2ce3cf037b
nimabandi <nimabandi0935@gmail.com>
2022-01-23 10:52:56 -0800

    add MP.py

---
+++ MP.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import pygame as pg
+
+root = tk.Tk()  # make instance of tkinter
+root.title('Music Player')  # title of root
+root.geometry('600x400')  # select the of root
+root.resizable(False, False)  # fixed the size
+
+root.mainloop()
