5bea87e19e81a9cb701fb20019b7ba0f0b76c44c
nimabandi <nimabandi0935@gmail.com>
2022-01-24 09:52:40 -0800

    changed MP play_list

--- MP.py
+++ MP.py
@@ -9,11 +9,21 @@ root.resizable(False, False)  # fixed the size
 
 # Play List
 
+songFrame = tk.LabelFrame(root, text='Song Play List', bg='black', fg='white', font=('Arial', 10), bd=5,
+                          relief=tk.GROOVE)
+songFrame.place(x=10, y=1, width=580, height=210)
+
+scrolly = tk.Scrollbar(songFrame, orient=tk.VERTICAL)
+PlayList = tk.Listbox(songFrame, bg='silver', fg='black', relief=tk.GROOVE, font=('Arial', 5), selectmode=tk.SINGLE,
+                      selectbackground='black', height=100)
+scrolly.config(command=PlayList.yview)
+scrolly.pack(side=tk.RIGHT, fill=tk.Y)
+PlayList.pack(fill=tk.BOTH)
 
 # Song Track
 
 trackFrame = tk.LabelFrame(root, text='Song Track', bg='black', fg='white', font=('Arial', 10), bd=5, relief=tk.GROOVE)
-trackFrame.place(x=10, y=10, width=580, height=90)
+trackFrame.place(x=10, y=309, width=580, height=90)
 
 showSongName = tk.Text(trackFrame, bg='white', fg='red', width=50, height=1, state=tk.DISABLED)
 showSongName.grid(row=0, column=0, padx=10, pady=20)
@@ -24,7 +34,7 @@ showStatus.grid(row=0, column=1, padx=20)
 # Control Panel
 Cpanel = tk.LabelFrame(root, text='Control Panel', bg='black', fg='white', font=('Arial', 10), bd=5, relief=tk.GROOVE,
                        padx=15)
-Cpanel.place(x=10, y=120, width=580, height=90)
+Cpanel.place(x=10, y=215, width=580, height=90)
 
 play = tk.Button(Cpanel, text='play', width=15)
 play.grid(row=0, column=0, padx=10, pady=20)
